0f3e9629f06af448edc673e433743f72cea10ad5
Jamie Yoon <rkawk3316@gmail.com>
2018-11-02 13:08:01 +0200

    Changed some stuff

--- task2/js/main.js
+++ task2/js/main.js
@@ -5,14 +5,11 @@ const lnameformat = /^[A-za-z]/;
 const email = document.querySelector("#email");
 const emailformat = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
 const phonenumber = document.querySelector("#phonenumber");
-const phoneformat = /^[+][0-9]{10}/;
+const phoneformat = /^(?:\+?3584|0)([0-9]){8}$/;
 const address = document.querySelector("#address");
-const addressformat = /^[A-za-z0-9]/;
 const postcode = document.querySelector("#postcode");
 const postformat = /^[0-9]{5}/;
 const password = document.querySelector("#password");
-const passwordformat = /^[A-za-z0-9]/;
-
 function validate() {
   if (firstname.value == "") {
     alert("Enter firstname");
@@ -27,7 +24,7 @@ function validate() {
     return false
   }
   else if (!email.value.match(emailformat)) {
-    alert("Wrong format");
+    alert("Wrong email format");
     return false
   }
   else if (phonenumber.value =="") {
@@ -35,23 +32,15 @@ function validate() {
     return false
   }
   else if (!phonenumber.value.match(phoneformat)) {
-    alert("Wrong format");
-    return false
-  }
-  else if (address.value !=addressformat) {
-    alert("Wrong format");
+    alert("Wrong phone number format");
     return false
   }
   else if (!postcode.value.match(postformat)) {
-    alert("Wrong format");
+    alert("Wrong postcode format");
     return false
   }
   else if (password.value =="") {
     alert("Enter password");
-    return false
-  }
-  else if (password.value!=passwordformat) {
-    alert("Wrong format");
     return false
   }
   else {
